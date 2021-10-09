9ab3c0140f35e74c4d72a9ce0c13f5e8c3e7169c
eee <hlxsmail@gmail.com>
2021-10-09 11:26:14 +0800

    add

--- src/request.ts
+++ src/request.ts
@@ -4,7 +4,7 @@ class Api {
 
   static instance(): DdServerApiByWeb {
     const webApi = new DdServerApiByWeb()
-    webApi.host = "http://192.168.100.15"
+    webApi.host = "https://itbug.shop"
     return webApi
   }
 
